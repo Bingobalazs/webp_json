a02f0a39c8c9ca365e4f156f542a730b5707e08b
Bingobalazs <baluvilaga@gmail.com>
2023-09-13 11:21:14 +0200

    s

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,46 @@
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    console.log("cooki bent");
-  }
+let osztalyadat=document.getElementById("osztalyadat")
+let tanulo=document.getElementById("tanulo")
+let lista=document.getElementById("listanez")
+let osztalynev=document.getElementById("osztalynev")
+let ofonev=document.getElementById("ofonev")
+let tannev=document.getElementById("tannev")
+let tanlak=document.getElementById("tanlak")
+let osztalyok=[];
 
-  function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
+function tagok(params) {
+  console.log("tagok f");
+  osztalyadat.style="display:none;"
+  lista.style="display:none;"
+  tanulo.style="display:block;"
 
-
-function send(){
-    setCookie("nev",document.getElementById("name").value, 10);
-    setCookie("kor",document.getElementById("kor").value, 10);
-    load();
-    console.log("kuldve");
+  osztalyok.push({
+    ofo:ofonev.value,
+    osztaly:osztalynev.value,
+    tagok:[]
+    
+  })
 }
-function load(params) {
-    document.getElementById("udv").innerHTML="Szia, "+getCookie("nev")+"! Boldog "+getCookie("kor")+". születésnapot";
-    console.log("töltve");
+function mentes(params) {
+osztalyok[osztalyok.length-1].tagok.push(
+  {nev: tannev.value, lakohely:tanlak.value})
+tannev.focus()
+}
+function osztaly(params) {
+  osztalyadat.style="display:block;"
+  lista.style="display:none;"
+  tanulo.style="display:none;"
+}
+function listaz(params) {
+  
+  osztalyadat.style="display:none;"
+  lista.style="display:block;"
+  tanulo.style="display:none;"
+  osztalyok.forEach(element => {
+    lista.innerHTML += "<ul><h1>"+element.osztaly+" "+element.ofo+"</h1></ul>";
+    let szamlalo =1;
+    element.tagok.forEach(tag => {
+      lista.innerHTML+= "<li>"+szamlalo+". Tanuló: "+tag.nev+"<ul><li> Lakóhelye: "+tag.lakohely+"</li></ul></li>"
+      szamlalo++;
+    });
+  });
 }
